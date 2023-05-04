5451e49e6310a83345d4c5e196964968dc16bd72
Alexandro Henrique Loch <alexsctj@gmail.com>
2023-05-04 09:12:37 -0300

    first commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import Post from "./Post"
+function App() {
+  return (
+    <>
+      <h1>Ola todos!</h1>
+      <Post />
+      <h1>Ola todos!</h1>
+      <Post />
+      <h1>Ola todos!</h1>
+      <Post />
+      <h1>Ola todos!</h1>
+      <Post />
+    </>
+  )
+}
+export default App
---
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,5 @@
+function Post(){
+  return <p>Post como vai você?</p>
+}
+
+export default Post
